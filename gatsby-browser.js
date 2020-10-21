aaa81fada7131b52570d721b09d669425901de14
Panayiotis Lipiridis <lipiridis@gmail.com>
2020-10-21 13:58:39 +0300

    Initial

---
+++ gatsby-browser.js
@@ -0,0 +1,18 @@
+const Countly = require("countly-sdk-web");
+
+const COUNTLY_API_KEY = "70907494988ecdd1093133ed565f5bdf61f96f34";
+let countlyLoaded = false;
+
+exports.onRouteUpdate = ({ location, prevLocation }) => {
+  if (!countlyLoaded) {
+    Countly.init({
+      app_key: COUNTLY_API_KEY,
+      debug: true,
+      device_id: "12345",
+      url: "https://countly.wire.com",
+    });
+    countlyLoaded = true;
+  }
+
+  Countly.track_pageview(location.pathname);
+};
